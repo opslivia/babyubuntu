ef15a7b08be9d35a7c6dabff153810264d8cd481
Olivia C <60422504+opslivia@users.noreply.github.com>
2021-11-07 22:19:58 -0800

    Update babyubuntu.tf

--- babyubuntu.tf
+++ babyubuntu.tf
@@ -54,7 +54,12 @@ resource "azurerm_linux_virtual_machine" "main" {
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
-
+  
+  admin_ssh_key {
+    username   = "oliviac"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }  
+  
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -66,4 +71,42 @@ resource "azurerm_linux_virtual_machine" "main" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "babyubuntu-pubip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "babyubuntu-secgroup"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
 }
